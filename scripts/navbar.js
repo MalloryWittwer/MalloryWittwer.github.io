390b1ec59604d41250eacb78f07b1c1c85af83e4
MalloryWittwer <mallory.wittwer@gmail.com>
2021-12-22 18:29:30 +0100

    Add responsiveness

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,11 +1,37 @@
-function openNav() {
-  document.getElementById("mySidebar").style.width = "150px";
-  document.getElementById("sidebar-btn").setAttribute('onclick', 'closeNav()');
-  document.getElementById("sidebar-btn").innerHTML = '<i class="fas fa-chevron-left"></i>'
+const openNav = () => {
+  document.getElementById("mySidebar").style.opacity = "0.9";
+  document.getElementById("sidebar-btn").style.opacity = "0";
+  document.getElementById("mySidebar").style.zIndex = "100";
+  document.getElementById("sidebar-btn").style.zIndex = "-1";
 }
 
-function closeNav() {
-  document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("sidebar-btn").setAttribute('onclick', 'openNav()');
-  document.getElementById("sidebar-btn").innerHTML = '&#9776;';
+const closeNav = () => {
+  document.getElementById("mySidebar").style.opacity = "0";
+  document.getElementById("sidebar-btn").style.opacity = "0.9";
+  document.getElementById("mySidebar").style.zIndex = "-1";
+  document.getElementById("sidebar-btn").style.zIndex = "100";
 }
+
+const closeNavDisplay = () => {
+  document.getElementById("mwittwer").style.display = "none";
+  document.getElementById("closenav").style.display = "block";
+  closeNav();
+}
+
+const openNavDisplay = () => {
+  document.getElementById("mwittwer").style.display = "block";
+  document.getElementById("closenav").style.display = "none";
+  openNav();
+}
+
+const mql = window.matchMedia("(max-width: 768px)");
+
+mql.onchange = (e) => {
+  if (e.matches) {
+    console.log('closing');
+    closeNavDisplay();
+  } else {
+    console.log('opening');
+    openNavDisplay();
+  }
+};
