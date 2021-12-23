0c5dec458d40a773b9f254d4d7a4babe38c40014
MalloryWittwer <mallory.wittwer@gmail.com>
2021-12-23 17:53:22 +0100

    Use rem as font-size

--- scripts/mobile-block-global.js
+++ scripts/mobile-block-global.js
@@ -1,4 +1,4 @@
-const manageMQ = (e) => {
+const manageMQ540 = (e) => {
   if (e.matches) {
     document.querySelector(".global-container").style.opacity = "0";
     document.querySelector(".global-container").style.zIndex = "-1";
@@ -12,8 +12,46 @@ const manageMQ = (e) => {
   }
 };
 
+const openNav = () => {
+  document.getElementById("mySidebar").style.opacity = "0.9";
+  document.getElementById("sidebar-btn").style.opacity = "0";
+  document.getElementById("mySidebar").style.zIndex = "100";
+  document.getElementById("sidebar-btn").style.zIndex = "-1";
+};
+
+const closeNav = () => {
+  document.getElementById("mySidebar").style.opacity = "0";
+  document.getElementById("sidebar-btn").style.opacity = "0.9";
+  document.getElementById("mySidebar").style.zIndex = "-1";
+  document.getElementById("sidebar-btn").style.zIndex = "100";
+};
+
+const closeNavDisplay = () => {
+  document.getElementById("mwittwer").style.display = "none";
+  document.getElementById("closenav").style.display = "block";
+  closeNav();
+};
+
+const openNavDisplay = () => {
+  document.getElementById("mwittwer").style.display = "block";
+  document.getElementById("closenav").style.display = "none";
+  openNav();
+};
+
+const manageMQ768 = (e) => {
+  if (e.matches) {
+    closeNavDisplay();
+  } else {
+    openNavDisplay();
+  }
+};
+
 const checkMQ = () => {
-  const mqlGlobal = window.matchMedia("(max-width: 540px)");
-  mqlGlobal.onchange = (e) => manageMQ(e);
-  document.addEventListener("DOMContentLoaded", manageMQ(mqlGlobal));
+  const mql540 = window.matchMedia("(max-width: 540px)");
+  mql540.onchange = (e) => manageMQ540(e);
+  document.addEventListener("DOMContentLoaded", manageMQ540(mql540));
+
+  const mql768 = window.matchMedia("(max-width: 768px)");
+  mql768.onchange = (e) => manageMQ768(e);
+  document.addEventListener("DOMContentLoaded", manageMQ768(mql768));
 };
